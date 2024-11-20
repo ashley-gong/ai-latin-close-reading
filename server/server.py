b522a0d47b49b7c54a0cd7b0cb90080b4bada226
ashley-gong <ashley.q.gong@gmail.com>
2024-11-20 02:00:24 -0500

    clean

--- server/server.py
+++ server/server.py
@@ -20,7 +20,7 @@ index = pc.Index("ai-latin-close-reading")
 
 tokenizer_path = 'models/subword_tokenizer_latin/latin.subword.encoder'
 bert_path = 'ashleygong03/bamman-burns-latin-bert'
-# bert = LatinBERT(tokenizerPath=tokenizer_path, bertPath=bert_path)
+bert = LatinBERT(tokenizerPath=tokenizer_path, bertPath=bert_path)
 
 
 @app.route("/api/home", methods=['GET'])
@@ -29,46 +29,47 @@ def return_home():
     'message': "Hello world!"
   })
 
-# def get_target_embedding(sentence, target_word):
-#     bert_sents = bert.get_berts([sentence])[0]
-#     for tok, embedding in bert_sents:
-#       if tok == target_word:
-#         return embedding / LA.norm(embedding)
-#     return None
+def get_target_embedding(sentence, target_word):
+    bert_sents = bert.get_berts([sentence])[0]
+    for tok, embedding in bert_sents:
+      if tok == target_word:
+        return embedding / LA.norm(embedding)
+    return None
 
-# @app.route('/api/query', methods=['POST'])
-# def query_similarity():
-#     data = request.json
-#     query_text = data.get("queryText")
-#     target_word = data.get("targetWord")
-#     num_results = int(data.get("numberResults")) if data.get("numberResults") != "" else 5
+@app.route('/api/query', methods=['POST'])
+def query_similarity():
+    data = request.json
+    query_text = data.get("queryText")
+    target_word = data.get("targetWord")
+    num_results = int(data.get("numberResults")) if data.get("numberResults") != "" else 5
 
-#     # target_embedding = get_target_embedding(query_text, target_word)
-#     bert_sents = bert.get_berts([query_text])[0]
-#     for tok, embedding in bert_sents:
-#       if tok == target_word:
-#         target_embedding = embedding / LA.norm(embedding)
-#         break
+    target_embedding = None #get_target_embedding(query_text, target_word)
+    bert_sents = bert.get_berts([query_text])[0]
+    for tok, embedding in bert_sents:
+      if tok == target_word:
+        target_embedding = embedding / LA.norm(embedding)
+        break
 
-#     target_embedding = target_embedding.tolist()
-#     if target_embedding is None:
-#         return jsonify({"error": "Target word not found in the sentence."}), 400
+    if target_embedding is None:
+        return jsonify({"error": "Target word not found in the sentence."}), 400
+  
+    target_embedding = target_embedding.tolist()
     
-#     if num_results > 30:
-#         return jsonify({"error": "Num results exceeds 30 (is too large)."}), 400
+    if num_results > 30:
+        return jsonify({"error": "Num results exceeds 30 (is too large)."}), 400
         
-#     results = index.query(vector=target_embedding, top_k=num_results, include_metadata=True)
-#     output = []
-#     for match in results['matches']:      
-#       output.append({
-#             "score": match['score'], 
-#             "token": match['metadata']['token'], 
-#             "document": match['metadata']['document'],
-#             "sentence": match['metadata']['sentence'],
-#             "section": match['metadata']['section']
-#           })
+    results = index.query(vector=target_embedding, top_k=num_results, include_metadata=True)
+    output = []
+    for match in results['matches']:      
+      output.append({
+            "score": match['score'], 
+            "token": match['metadata']['token'], 
+            "document": match['metadata']['document'],
+            "sentence": match['metadata']['sentence'],
+            "section": match['metadata']['section']
+          })
 
-#     return output
+    return output
 
 
 if __name__ == "__main__":
